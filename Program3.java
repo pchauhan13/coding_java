8681cd9c13e12d9a037975bbe4007ada5e7868b9
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-25 17:23:16 +0530

    program names made more consistent for the project

--- Program3.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Program3 {
-    public static void main(String[] args)
-    {
-        Date now = new Date();
-        System.out.println(now);
-    }
-}
---
+++ Program_5.java
@@ -0,0 +1,6 @@
+public class Program5 {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        System.out.println(message);
+    }
+}
