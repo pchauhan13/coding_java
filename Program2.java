1d57e3d8fceb0a8cdb672aee0375feb1a5a77dff
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-25 17:09:10 +0530

    program to declare primitive types added

---
+++ Program2.java
@@ -0,0 +1,11 @@
+public class Program2 {
+    public static void main(String[] args)
+    {
+        int age = 30;
+        byte a = 40;
+        long viewsCount = 3_324_434_434L;
+        float price = 4.444F;
+        char letter = 'A';
+        boolean isEligible = false;
+    }
+}
