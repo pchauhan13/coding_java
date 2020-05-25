5be6a04438bb547296de975a696e1673f873c5d3
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-25 18:06:23 +0530

    program to take primitive data type input from the user

---
+++ Program_8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter an integer");
+        int i = scanner.nextInt();
+        System.out.println(i);
+    }
+}
