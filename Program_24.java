83b384c1ece66bb6c4265b3998d6734386269243
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 17:53:51 +0530

    program to define and intialize array

---
+++ Program_24.java
@@ -0,0 +1,7 @@
+public class Program_24 {
+    public static void main(String[] args) {
+        int[] grades = new int[10];
+        grades[0] = 10;
+        System.out.println(grades[0]);
+    }
+}
