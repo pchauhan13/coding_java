4caa13a65a11678b07cf64f67775602467dc3c89
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 08:15:34 +0530

    program to use numeric expressions and operators

---
+++ Program_11.java
@@ -0,0 +1,23 @@
+public class Program_11 {
+    public static void main(String[] args) {
+        int a = 5;
+        double d = (double) a / 2;
+        System.out.println(d);
+
+        int b = 2;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x--);
+
+        x += 1;
+        x -= 1;
+        x *= 1;
+        x /= 1;
+    }
+}
