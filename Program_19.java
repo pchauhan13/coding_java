d3e23ad7e16a4c6328b66904002a616ccc8787e9
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 15:28:50 +0530

    program to use ternary operator

---
+++ Program_19.java
@@ -0,0 +1,7 @@
+public class Program_19 {
+    public static void main(String[] args) {
+        String name = "Claire";
+        int points = name.equals("Claire") ? 5 : 0;
+        System.out.println(points);
+    }
+}
