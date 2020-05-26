630fe430e8bd8eacd38d28f50bdb50af48b9009e
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 08:01:51 +0530

    program to cast double value to int value type

---
+++ Program_9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program_9 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = (int)scan.nextDouble();
+        System.out.println(i);
+
+        scan.close();
+    }
+}
