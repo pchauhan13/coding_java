798c5eedd5754b44b179deb14bc92e81af9bdbc6
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 18:02:46 +0530

    program to print 2d arrays

---
+++ Program_27.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Program_27 {
+    public static void main(final String[] args) {
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6, 7, 8 } };
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
