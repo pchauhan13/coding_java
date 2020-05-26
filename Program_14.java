1f63e987c84819428f269c9dbe270a1798b2cdb1
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 11:26:46 +0530

    program to create a basic class with a property

---
+++ Program_14.java
@@ -0,0 +1,11 @@
+class User {
+    String name;
+}
+
+public class Program_14 {
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "Prashant";
+        System.out.println(user);
+    }
+}
