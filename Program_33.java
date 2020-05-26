774b203a5694286d440f396a6e992494ba810c19
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 19:34:33 +0530

    program to define 2d array and apply methods on it

---
+++ Program_33.java
@@ -0,0 +1,10 @@
+public class Program_33 {
+    public static void main(String[] args) {
+        int[][] grades = {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+        System.out.println(grades.length);
+        System.out.println(grades[0].length);
+    }
+}
