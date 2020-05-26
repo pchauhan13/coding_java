ad428989bc184e0d6537123bdcebc46060ea9ddd
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 17:27:22 +0530

    program to use do..while loop

---
+++ Program_21.java
@@ -0,0 +1,10 @@
+public class Program_21 {
+    public static void main(final String[] args) {
+        int i = 0;
+
+        do
+        {
+            System.out.println(i++);
+        } while (i < 10);
+    }
+}
