0f4720db56498d7aab089a4604d8f03aaeb7806b
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-26 11:29:05 +0530

    access method added to user class

--- Program_14.java
+++ Program_14.java
@@ -1,11 +1,19 @@
 class User {
-    String name;
+    public String firstName;
+    public String lastName;
+
+    public String getFullName ()
+    {
+        return this.firstName + " " + this.lastName;
+    }
+
 }
 
 public class Program_14 {
     public static void main(String[] args) {
         User user = new User();
-        user.name = "Prashant";
-        System.out.println(user);
+        user.firstName = "Prashant";
+        user.lastName = "Singh Chauhan";
+        System.out.println(user.getFullName());
     }
 }
