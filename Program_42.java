bcbf1f237bad8cbb0cec439b8b5332819c145f60
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-27 08:37:18 +0530

    program to define enum and using it

---
+++ Program_42.java
@@ -0,0 +1,7 @@
+public class Program_42 {
+    enum status { active, inactive };
+    public static void main(String[] args) {
+        status s = status.active;
+        System.out.println(s);
+    }
+}
