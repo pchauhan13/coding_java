09c2d2d23eb4896d5fb4c045563272661f06bd2f
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-09 10:17:40 +0530

    creating a hashmap and using it

--- Program_46.java
+++ Program_46.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+
 public class Program_46 {
     public static void main(String[] args) {
         System.out.println("hello, world!".hashCode());
+
+        HashMap<String, Integer> ids = new HashMap<>();
+
+        ids.put("Prashant", 4);
+        ids.put("Caleb", 4);
+
+        System.out.println(ids.get("Prashant"));
     }
 }
